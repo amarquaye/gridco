481206e298c840c00c11874e64c7c2a0fd276056
Jesse Amarquaye <96346994+amarquaye@users.noreply.github.com>
2023-12-02 16:45:50 +0000

    Added README and updated codebase

--- week2/word_search/main.py
+++ week2/word_search/main.py
@@ -7,13 +7,16 @@ def main():
     word = sys.argv[1]
     file = sys.argv[2]
 
-    # Open file in read mode
-    with open(file, "r") as filename:
-        contents = filename.read()
-        if word in contents:
-            print("Yes")
-        else:
-            print("No")
+    try:
+        # Open file in read mode
+        with open(file, "r") as filename:
+            contents = filename.read()
+            if word in contents:
+                print(f"Yes, {word} is in {file}")
+            else:
+                print(f"No, cannot find {word} in {file}")
+    except Exception as e:
+        print(f"Error, {e}")
 
 
 if __name__ == "__main__":
