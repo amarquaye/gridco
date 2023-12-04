11f3ef440fd01b5efd6b23fe42bfd4ddb6cdb343
Daniel Tamakloe <dansey2002@outlook.com>
2023-12-04 00:00:32 +0000

    Array for the search now shows

--- week3/LinearSearch/main.cpp
+++ week3/LinearSearch/main.cpp
@@ -4,9 +4,14 @@ int main(){
     int arr[] = {1, 2, 3, 4, 5, 6, 9, 0, 21};
     int arrsize = sizeof arr/sizeof arr[0];
 
+    for(int i = 0; i < arrsize; i++){
+        std::cout << arr[i] << " " ;
+    }
+
     int num;
-    std::cout << "Enter a number to search for: ";
+    std::cout << "\n Enter a number to search for: ";
     std::cin >> num;
+
 
     for(int i = 0; i < arrsize; i++){
         if(num == arr[i]){
