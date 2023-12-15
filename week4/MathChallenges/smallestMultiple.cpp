930fffd4fd0ed92cb9a45d1b9ca603ebf45416ee
Daniel Tamakloe <dansey2002@outlook.com>
2023-12-15 10:12:34 +0000

    LCM done

--- week4/MathChallenges/smallestMultiple.cpp
+++ week4/MathChallenges/smallestMultiple.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 
-int main(){
-    for(int i = 0; i < 1000; i++){
-        if(i){}
+#include <iostream>
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int remainder = a % b;
+        a = b;
+        b = remainder;
     }
+    return a;
+}
+
+int lcm(int start, int end){
+    int result = 1;
+    for(int i = start; i <= end; i++){
+        result = result * (i/gcd(i, result));
+    }
+    return result;
+}
+
+int main(){
+    int smallestMultiple = lcm(1, 20);
+    std::cout << smallestMultiple;
 }
